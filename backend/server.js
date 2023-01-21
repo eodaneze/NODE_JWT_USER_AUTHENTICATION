7045b33260b83bfe99fdcea84d8b174f96649824
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-01-21 05:37:20 -0800

    installing jsonwebtoken and cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const router = require("./routes/userRouter");
-require("./dbconnect/connection")
+require("./dbconnect/connection");
+const cookieParser = require("cookie-parser")
 dotenv.config();
 const app = express();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api", router)
-const port = process.env.PORT || 8000
-app.listen(port, ()=> {
-    console.log(`server is running on port on http://localhost:${port}`);
-})
+app.use("/api", router);
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`server is running on port on http://localhost:${port}`);
+});
