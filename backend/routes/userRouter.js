1861bf5c37d64081c46a7679f7a1f5939121352d
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-01-21 07:26:07 -0800

    creating a conroller

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -2,58 +2,14 @@ const router = require("express").Router();
 const Users = require("../models/userModels");
 const bcrypt = require("bcryptjs");
 const {createToken, validateToken} = require("../auth/token");
+const { signUpUser, loginUser } = require("../controller/controller");
 // regustring a user
 
-router.post("/signup", async (req, res) => {
-  const { name, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password);
-  try {
-    const user = await Users({
-      name,
-      email,
-      password: hashedPassword,
-    });
-    const data = await user.save();
-    user &&
-      res.json({
-        message: "Your account has been created successfully",
-        result: data,
-      });
-  } catch (err) {
-    res.status(400).json({ error: err });
-  }
-});
+router.post("/signup", signUpUser);
 
 // login user
 
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  let existingUser;
-
-  try {
-    existingUser = await Users.findOne({ email });
-    if (!existingUser) {
-      res.status(404).json({ message: "User does not exist" });
-    }
-
-    const comparePassword = bcrypt.compareSync(password, existingUser.password);
-
-    if (!comparePassword) {
-      res.status(404).json({ message: "Password is wrong" });
-    } else {
-      const accessToken = createToken(existingUser);
-      res.cookie("your-access-token", accessToken, {
-         maxAge: 60 * 60 * 24 * 30 * 1000,
-      })
-      res.json({
-        message: "You have logged in successfully",
-        result: existingUser,
-      });
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
+router.post("/login", loginUser);
 
 router.get("/profile", validateToken, (req, res) => {
     res.send("This is user profile")
